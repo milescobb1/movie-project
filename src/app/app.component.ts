27ee8465b0899f489744813a50f25a7dc69740e1
milescobb1 <miles.cobb@gmail.com>
2018-02-08 08:37:13 -0800

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('movies', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: false}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: false})
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  movie: string;
+  movies = [];
+  lists = {'zero': null, 'one': ['1','2','3'], 'two':['a','b','c'] }
+  listNames = []
+  currentList: string;
+
+  ngOnInit() {
+    this.currentList = Object.keys(this.lists)[0];
+    this.movies = this.lists[this.currentList];
+    this.listNames = Object.keys(this.lists);
+    console.log(Object.keys(this.lists));
+  }
+
+  loadList(name) {
+    this.movies = this.lists[name]
+    console.log(this.lists[name]);
+    this.currentList = name;
+  }
+
+  addItem() {
+    if(this.movie != null && this.movie != '') {
+      console.log('called');
+      if (this.lists[this.currentList])
+        this.lists[this.currentList].push(this.movie)
+      else {
+        this.lists[this.currentList] = [this.movie]
+        this.movies = this.lists[this.currentList]
+        this.listNames.push(this.currentList)
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
+import { MoviesComponent } from './movies/movies.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
